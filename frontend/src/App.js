1390979b46527684d18f02d0c67e874489a0baf4
VictorBK <victorbrian4@gmail.com>
2023-03-17 22:14:16 +0300

    Initial frontend files commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-green-500'>
+      React app
+    </div>
+        
+  );
+}
+
+export default App;
