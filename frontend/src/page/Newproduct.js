1fe6ea401ff31a37fb07b874440f81fb0b9b1972
VictorBK <victorbrian4@gmail.com>
2023-03-20 02:32:29 +0300

    Initial New Product page changes

--- frontend/src/page/Newproduct.js
+++ frontend/src/page/Newproduct.js
@@ -1,8 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'react-hot-toast'
+import {BsUpload} from "react-icons/bs"
+import { ImagetoBase64 } from '../utility/ImagetoBase64'
 
 const Newproduct = () => {
+  const [data,setData] = useState({
+    name : "",
+    category : "",
+    image : "",
+    price : "",
+    description : ""
+  })
+
+  const handleOnChange = (e)=>{
+    const {name,value} = e.target
+
+    setData((preve)=>{
+        return{
+          ...preve,
+          [name] : value
+        }
+    })
+
+  }
+
+  const uploadImage = async(e)=>{
+      const data = await ImagetoBase64(e.target.files[0])
+     // console.log(data)
+
+     setData((preve)=>{
+      return{
+        ...preve,
+        image : data
+      }
+     })
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+
+    console.log(data)
+  }
   return (
-    <div>Newproduct</div>
+    <div className='p-4'>
+      <form className='m-auto w-full max-w-md shadow flex flex-col p-3 bg-white' onSubmit={handleSubmit}> 
+        <label htmlFor='name'>Name</label>
+        <input type={"text"} name="name" className='bg-slate-200 p-1 my-1' onChange={handleOnChange}/>
+
+        <label htmlFor='category'>Category</label>
+        <select className='bg-slate-200 p-1 my-1' id='category' name='category' onChange={handleOnChange}>
+          <option>Fruits</option>
+          <option>Vegetables</option>
+          <option>Meats</option>
+          <option>Spices</option>
+          <option>Others</option>
+        </select>
+
+        <label htmlFor='image'>Image
+        <div className='h-40 w-full bg-slate-200 rounded flex items-center justify-center cursor-pointer'>
+            {
+              data.image ? <img src={data.image} className="h-full" /> : <span className='text-5xl'><BsUpload/></span> 
+            }
+            
+                    
+           <input type={"file"} accept="image/*" id="image" onChange={uploadImage} className="hidden"/>
+        </div>
+        </label>
+
+        <label htmlFor='price' className='my-1'>Price</label>
+        <input type={"text"} className='bg-slate-200 p-1 my-1' name='price' onChange={handleOnChange}/>
+
+        <label htmlFor='description'>Description</label>
+        <textarea rows={4} className='bg-slate-200 p-1 my-1 resize-none' name='description' onChange={handleOnChange}></textarea>
+
+        <button className='bg-green-400 hover:bg-green-600 text-white text-lg font-medium my-2 drop-shadow'>Save</button>
+      </form>
+    </div>
   )
 }
 
