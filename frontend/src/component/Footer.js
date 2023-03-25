6de503ded49f47b9d8a56c55c00bfa86c71d1f0c
VictorBK <victorbrian4@gmail.com>
2023-03-25 22:17:22 +0300

    Footer innit

---
+++ frontend/src/component/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return <section className='bg-slate-400'>
+    <footer className='container mx-auto grid grid-cols-10'>
+
+    </footer>
+
+  </section>
+  
+}
+
+export default Footer
