9008a418319d4a9a351abb7b2f7fa20999e775c6
VictorBK <victorbrian4@gmail.com>
2023-03-25 22:17:50 +0300

    Menu changes

--- frontend/src/page/Menu.js
+++ frontend/src/page/Menu.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { UseSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
 const Menu = () => {
-  const params = useParams()
-  console.log(params.filterby)
+  const {filterby} = useParams()
+  const productData = useSelector(state => state.product.productList)
+
+  const productDisplay = productData.filter(el => el._id === filterby)
+  console.log(productDisplay)
   return (
     <div>Menu</div>
   )
