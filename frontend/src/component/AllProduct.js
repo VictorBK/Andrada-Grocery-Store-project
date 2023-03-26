8108e411a28fd33a8e3ca6b70eee4fd55c18986a
VictorBK <victorbrian4@gmail.com>
2023-03-26 21:24:31 +0300

    All Product tentative changes

--- frontend/src/component/AllProduct.js
+++ frontend/src/component/AllProduct.js
@@ -1,76 +1,73 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import CardFeature from "./CardFeature";
-import FilterProduct from "./FilterProduct";
+import React, { useEffect, useState } from 'react'
+import FilterProduct from './FilterProduct'
+import CardFeature from './CardFeature'
+import { useSelector } from 'react-redux'
 
-const AllProduct = ({ heading }) => {
+const AllProduct = ({heading, loading}) => {
   const productData = useSelector((state) => state.product.productList);
-  const categoryList = [...new Set(productData.map((el) => el.category))];
+  const categoryList = [...new Set(productData.map(el=>el.category))]
+  console.log(categoryList)
 
-  //filter data display
-  const [filterby, setFilterBy] = useState("");
-  const [dataFilter, setDataFilter] = useState([]);
-
-  useEffect(() => {
-    setDataFilter(productData);
-  }, [productData]);
-
-  const handleFilterProduct = (category) => {
-    setFilterBy(category)
+   // filter data display
+  const [filterby,setFilterBy] = useState("")
+  const [dataFilter,setDataFilter] = useState([])
+ 
+  useEffect(()=>{
+    setDataFilter(productData)
+  },[productData])
+ 
+  const handleFilterProduct = (category)=>{
     const filter = productData.filter(
       (el) => el.category.toLowerCase() === category.toLowerCase()
-    );
-    setDataFilter(() => {
-      return [...filter];
-    });
-  };
+    )
+     setDataFilter(()=>{
+       return[
+         ...filter
+       ]
+     })
+   }
 
   const loadingArrayFeature = new Array(10).fill(null);
 
   return (
     <div className="my-5">
-      <h2 className="font-bold text-2xl text-slate-800 mb-4">{heading}</h2>
+    <h2 className="font-bold text-2xl text-slate-800 mb-4">{heading}</h2>
 
-      <div className="flex gap-4 justify-center overflow-scroll scrollbar-none">
-        {categoryList[0] ? (
-          categoryList.map((el) => {
-            return (
-              <FilterProduct
-                category={el}
-                key={el}
-                isActive={el.toLowerCase() === filterby.toLowerCase()}
-                onClick={() => handleFilterProduct(el)}
-              />
-            );
-          })
-        ) : (
-          <div className="min-h-[150px] flex justify-center items-center">
-            <p>Loading...</p>
-          </div>
-        )}
-      </div>
-
-      <div className="flex flex-wrap justify-center gap-4 my-4">
-        {dataFilter[0]
-          ? dataFilter.map((el) => {
-              return (
+    <div className="flex gap-4 justify-center overflow-scroll scrollbar-none">
+          {categoryList[0] ? (
+             categoryList.map(el =>{
+              return(
+                <FilterProduct 
+                  category={el} 
+                  key={el}
+                  onClick={()=>handleFilterProduct(el)}/>
+              )
+            })
+          ) : (
+            <div className='min-h-[150px] flex justify-center items-center'>
+              <p>Loading...</p>
+            </div>
+          )}     
+    </div>
+    <div className="flex flex-wrap justify-center gap-4 my-4">
+          {dataFilter[0] ? dataFilter.map((el) => {
+              return(
                 <CardFeature
                   key={el._id}
                   id={el._id}
                   image={el.image}
                   name={el.name}
                   category={el.category}
-                  price={el.price}
+                  price={el.price}                    
                 />
-              );
+              )
             })
-          : 
-          loadingArrayFeature.map((el,index) => (
-              <CardFeature loading="Loading..." key={index+"allProduct"} />
+            : loadingArrayFeature.map((el,index) => (
+                <CardFeature loading="Loading..." key={index+"allProduct"} />
             ))}
-      </div>
     </div>
-  );
-};
+  </div>
+  )
+}
 
-export default AllProduct;
+export default AllProduct
