bd246067c0df3a514de0b7fd2d9faaf58ebcde50
VictorBK <victorbrian4@gmail.com>
2023-03-27 03:11:34 +0300

    Home changes

--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -44,7 +44,7 @@ const Home = () => {
             <p className='text-sm font-medium text-slate-900'>Organic Choices</p>
             <img src='https://www.healthkart.com/connect/wp-content/uploads/2022/07/900x500_thumbnail_HK-how-to-have-a-balanced-diet-1.png' className='h-7'/>
           </div>
-          <h2 className='text-4xl md:text-7xl font-bold py-3'>The <span className='text-green-500 '> Healthiest</span> Choice in <span className='text-red-600 '>Town</span></h2>
+          <h2 className='text-4xl md:text-7xl font-bold py-3'>The <span className='text-green-500 '> Healthiest</span> Choice in{" "}<span className='text-red-600 '>Town</span></h2>
           <p className='py-3 text-base'> Our mission is to change the way you eat.
                                          By connecting you to organically grown and bred meal options in categories such as fruits, vegetables, meats, cereals, spices, and other healthy choices, we trust that you shall appreciate the dedication and passion that our producers put into their work to ensure that you are provided with an array of the healthiest meal options in town. 
                                          We believe that everyone has a right to access organically grown food at a time when individuals still battle with the safety of chemically-grown and bred food options. Our heroes are the subsistent farmers who choose to adopt organic farming no matter the time it takes to get the food and meal options ready for the market.
